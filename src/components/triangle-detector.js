55fadd4bcbd890ad62de509e9e281d1e6fb6468d
Lisa Felder <feldmuesle@yahoo.com>
2018-09-15 16:22:11 +0200

    validate if input value is number, setup handle-submit-func

--- src/components/triangle-detector.js
+++ src/components/triangle-detector.js
@@ -14,17 +14,25 @@ class TriangleDetektor extends Component {
 
 	handleInput(event) {
 		const input = event.target;
-
 		this.setState({
 			[input.name]: input.value
 		});
+	}
+
+	handleSubmit(event) {
+		console.log('handle submit');
+		event.preventDefault();
+	}
+
+	validateIfValueIsNumber(value) {
+		return value.length && isNaN(parseInt(value, 10));
 	}
 
 	render() {
 		return (
 			<React.Fragment>
 				<h1>Determine type of triangle</h1>
-				<form data-ts="Form">
+				<form data-ts="Form" onSubmit={(event)=>{this.handleSubmit(event);}}>
 					<fieldset>
 					<Input
 						label="side A"
@@ -33,6 +41,10 @@ class TriangleDetektor extends Component {
 						value={this.state.sideA}
 						placeholder="enter length of side A"
 						handleInput={this.handleInput}/>
+					{this.validateIfValueIsNumber(this.state.sideA) &&
+						<dl className="ts-errors">
+							<dt>Value must be a number</dt>
+						</dl>}
 					<Input
 						label="side B"
 						type="text"
@@ -40,6 +52,10 @@ class TriangleDetektor extends Component {
 						value={this.state.sideB}
 						placeholder="enter length of side B"
 						handleInput={this.handleInput}/>
+					{this.validateIfValueIsNumber(this.state.sideB) &&
+						<dl className="ts-errors">
+							<dt>Value must be a number</dt>
+						</dl>}
 					<Input
 						label="side C"
 						type="text"
@@ -47,8 +63,12 @@ class TriangleDetektor extends Component {
 						value={this.state.sideC}
 						placeholder="enter length of side C"
 						handleInput={this.handleInput}/>
+					{this.validateIfValueIsNumber(this.state.sideC) &&
+						<dl className="ts-errors">
+							<dt>Value must be a number</dt>
+						</dl>}
 					</fieldset>
-					<button data-ts="Button" className="ts-primary">
+					<button data-ts="Button" className="ts-primary" onClick={(event)=>{this.handleSubmit(event);}}>
 						<span>submit</span>
 					</button>
 				</form>
