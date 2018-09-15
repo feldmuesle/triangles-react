5b2f7df8d0e4367e0bf0ac8c220227c16f8f52ae
Lisa Felder <feldmuesle@yahoo.com>
2018-09-15 14:41:55 +0200

    added form-elements

--- src/components/triangle-detector.js
+++ src/components/triangle-detector.js
@@ -7,7 +7,30 @@ class TriangleDetektor extends Component {
 	}
 
 	render() {
-		return <div>Hello from TriangleDetektor!!</div>;
+		return (
+			<React.Fragment>
+				<h1>Determine type of triangle</h1>
+				<form data-ts="Form">
+					<fieldset>
+						<label>
+							<span>side A</span>
+							<input type="number" placeholder="enter length of side A"/>
+						</label>
+						<label>
+							<span>side B</span>
+							<input type="number" placeholder="enter length of side B"/>
+						</label>
+						<label>
+							<span>side C</span>
+							<input type="number" placeholder="enter length of side C"/>
+						</label>
+					</fieldset>
+					<button data-ts="Button" className="ts-primary">
+						<span>submit</span>
+					</button>
+				</form>
+			</React.Fragment>
+		);
 	};
 
 };
